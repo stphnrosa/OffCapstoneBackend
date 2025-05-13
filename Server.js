4998ab287ae26e32dc9b521cad460e68ae99e668
stphnrosa <stphn.rosa@gmail.com>
2025-05-13 19:54:40 -0400

    server updated

--- Server.js
+++ Server.js
@@ -1,6 +1,8 @@
 import { config } from 'dotenv'; config();
 import express from 'express'; // added to package.json "type":"module", to allow import express instead of const express.Can't use import/export unless you do this.
 import cors from 'cors'; // cors is used to have communication between Backend and Frontend
+import router  from './Routes/BlogRoute';
+import './Database/Database'
 
 //const declared the variable "app". we placed () to invoke express after importing it on line 2
 const app = express();
@@ -10,6 +12,7 @@ const PORT= process.env.PORT || 3000; // user could still run the server with th
 app.use(express.urlencoded({ extended: true}))
 app.use(express.json());
 app.use(cors());
+app.use('/blog', router);
 
 
 app.listen(PORT, () => {
